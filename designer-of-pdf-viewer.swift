06b7f3518ddc2c0d89aae8c72c6c4dcc637b961b
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-14 23:34:56 +0700

    add input and output desc

--- designer-of-pdf-viewer.swift
+++ designer-of-pdf-viewer.swift
@@ -1,5 +1,6 @@
 // problem https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
-
+// input : h : [Int], word: String
+// output : areas : Int 
 
 
 func designerPdfViewer(h: [Int], word: String) -> Int {
