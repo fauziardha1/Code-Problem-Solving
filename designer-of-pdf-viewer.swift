730d82e3636cabb54597725ece39488022552f97
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-14 23:33:59 +0700

    add unction designeerPDfViewer

--- designer-of-pdf-viewer.swift
+++ designer-of-pdf-viewer.swift
@@ -1,1 +1,12 @@
 // problem https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
+
+
+
+func designerPdfViewer(h: [Int], word: String) -> Int {
+    // Write your code here
+    var values = [Int]()
+    for char in Array(word.lowercased()){
+        values.append(h[Int(char.asciiValue!) - 97])
+    }
+    return values.max()! * values.count
+}
