0a14ca6a22cce49ae59cd6518b3efeaf2a55e3b0
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-15 21:33:36 +0700

    add desc and solution

--- utopian-tree.swift
+++ utopian-tree.swift
@@ -1,1 +1,14 @@
 // problem : https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
+// input : n:Int (cycle)
+// output : height : Int
+
+print(utopianTree(n:5))
+
+func utopianTree(n: Int) -> Int {
+    // Write your code here
+    var height = 1
+    for num in 0..<n {
+        height += num%2==0 ? height : 1
+    }
+    return height
+}
