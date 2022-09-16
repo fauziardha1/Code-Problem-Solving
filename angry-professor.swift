a17f1ed54370444adc62837d27a60596619ed1c1
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-16 23:14:21 +0700

    solution

--- angry-professor.swift
+++ angry-professor.swift
@@ -1,3 +1,12 @@
 // problem : https://www.hackerrank.com/challenges/angry-professor/problem?isFullScreen=true
 // input : k: Int, a: [Int]
 // output : String
+
+func angryProfessor(k: Int, a: [Int]) -> String {
+    // Write your code here
+    var count = 0;
+    for num in a {
+        count += num <= 0 ? 1 : 0
+    }
+    return count < k ? "YES" : "NO"
+}
