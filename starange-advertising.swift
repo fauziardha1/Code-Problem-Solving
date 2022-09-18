85b134ea13f2638628c12b9902830657d7b55f8a
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-18 21:38:30 +0700

    add solution

--- starange-advertising.swift
+++ starange-advertising.swift
@@ -1,1 +1,16 @@
 // problem : https://www.hackerrank.com/challenges/strange-advertising/problem?isFullScreen=true
+// input : n Int
+// output : cumulative result : Int
+
+func viralAdvertising(n: Int) -> Int {
+    // Write your code here
+    var  shared = 5;
+    var liked = 0;
+    var cummulative = 0;
+        for idx in 0..<n {
+            liked = shared/2;
+            cummulative += liked;
+            shared = liked * 3;
+        }
+        return cummulative;
+}
