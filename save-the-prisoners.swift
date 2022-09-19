b2d2ba473709041bbe74fa9595b3368ea8701d42
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-19 19:51:18 +0700

    add solution

--- save-the-prisoners.swift
+++ save-the-prisoners.swift
@@ -1,1 +1,7 @@
 // problem : https://www.hackerrank.com/challenges/save-the-prisoner/problem?isFullScreen=true
+
+func saveThePrisoner(n: Int, m: Int, s: Int) -> Int {
+    // Write your code here
+    let result = (m+s-1)%n 
+    return result == 0 ? n : result 
+}
