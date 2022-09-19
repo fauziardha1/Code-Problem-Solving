fed1ade5f7958c0d714afbff59f3ea1339f1a279
fauziardha1 <fauziardasaputra@gmail.com>
2022-09-19 20:05:25 +0700

    add desc

--- save-the-prisoners.swift
+++ save-the-prisoners.swift
@@ -1,4 +1,6 @@
 // problem : https://www.hackerrank.com/challenges/save-the-prisoner/problem?isFullScreen=true
+// input : n is number of prisoner, m is number of candy, s is index for start
+// output :  index of prisoner that get the last candy
 
 func saveThePrisoner(n: Int, m: Int, s: Int) -> Int {
     // Write your code here
